523cf738b0b58fa20c4ec97ebf5bf9a0152acc55
FMoller <fredericomollerper@gmail.com>
2021-04-05 21:55:13 -0300

    Update cgp_classes.py

--- minicurso/cgp_classes.py
+++ minicurso/cgp_classes.py
@@ -10,7 +10,7 @@ __license__ = "MIT"
 import numpy as np
 
 class node():
-    """ A cgp node"""
+    """ A cgp node """
 
     def __init__(self, pos, levels_back, lines, n_gates, inputs):
 
@@ -24,6 +24,36 @@ class node():
                        self.values[np.random.randint(len(self.values))]],)
         self.active = False
         self.output = -1
+
+    def get_node(self):
+        """ print the node"""
+        return "{"+str(self.i_add)+" , "+str(self.gate)+"}"
+
+class individual():
+    """ A cgp individual """
+
+    def __init__(self, inputs, outputs, lines, cols, levels_back, n_gates):
+
+        self.nodes = []
+        for i in range(lines):
+            self.nodes.append([])
+            for j in range(cols):
+                self.nodes[i].append(node(j, levels_back, lines, n_gates, inputs))
+        self.cols = cols
+        self.lines = lines
+
+    def print(self):
+        """ print the individual"""
+        for j in range(self.cols):
+            expr = "col "+str(j)+": "
+            for i in range(self.lines):
+                expr += self.nodes[i][j].get_node()+"\t"
+            print(expr)
+            
+        
+    
+
+        
         
                        
     
